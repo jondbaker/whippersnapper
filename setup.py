c194e2c9d98efcdd1b5e3e0a11de0aafeadc1648
Jonathan D. Baker <jonathan@piqueinteractive.com>
2013-01-10 11:26:15 -0700

    Define python setup attributes.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="whippersnapper",
+    version="0.1.0",
+    description="",
+    long_description=open("README.rst").read(),
+    author="Jonathan D. Baker",
+    author_email="jonathan@piqueinteractive.com",
+    url="https://github.com/jondbaker/whippersnapper",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
