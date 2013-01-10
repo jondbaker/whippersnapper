8bf33ed3795013ad2f57c0dea70bada0529747e5
Jonathan D. Baker <jonathan@piqueinteractive.com>
2013-01-10 16:27:02 -0700

    Write tests for promiscuity mode.

--- whippersnapper/tests/unit.py
+++ whippersnapper/tests/unit.py
@@ -22,23 +22,28 @@ class TestBuildDictionary(BaseTestCase):
         super(TestBuildDictionary, self).setUp()
 
     def test_empty_source(self):
-        result = self.parser.build_dictionary(
-            "./tests/fixtures/empty.txt", False)
+        result = self.parser.build_dictionary("./tests/fixtures/empty.txt")
         self.assertEqual(result, {})
 
     def test_first_entry(self):
         result = self.parser.build_dictionary(
-            "./tests/fixtures/dictionary.txt", False)
+            "./tests/fixtures/dictionary.txt")
         self.assertEqual(result["459"], "I Love You")
 
     def test_last_entry(self):
         result = self.parser.build_dictionary(
-            "./tests/fixtures/dictionary.txt", False)
+            "./tests/fixtures/dictionary.txt")
         self.assertEqual(
             result[":%I"], "Lowering glasses to see if you're serious")
 
     def test_promiscuous(self):
-        pass
+        parser = MessageParser(self.MESSAGE, True)
+        result = parser.build_dictionary(
+            "./tests/fixtures/dictionary.txt")
+        self.assertEqual(
+            result[":%i"], "Lowering glasses to see if you're serious")
+        self.assertEqual(
+            result["cu"], "See You")
 
 
 class TestParse(BaseTestCase):
@@ -46,10 +51,16 @@ class TestParse(BaseTestCase):
     def setUp(self):
         super(TestParse, self).setUp()
 
-    def test_expected_result(self):
+    def test_result(self):
         result = self.parser.parse()
         expected = "Hi Grandma! I Love You! Can't wait to See You Happy. Just Curious if You got my thank you card for the You Too tickets: they are SOOOO awesome! Hugs and Kisses, Lola"
         self.assertEqual(result, expected)
+
+    def test_promiscuous(self):
+        message = "459 xOxO ql"
+        parser = MessageParser(message, True)
+        result = parser.parse()
+        self.assertEqual(result, "I Love You Hugs and Kisses Quit Laughing")
 
 
 class TestEdgeOfSentenceSegment(BaseTestCase):
@@ -110,6 +121,11 @@ class TestSubstitute(BaseTestCase):
     def test_word(self):
         result = self.parser.substitute("Howdy")
         self.assertEqual(result, "Howdy")
+
+    def test_promiscuous(self):
+        parser = MessageParser(self.MESSAGE, True)
+        result = parser.substitute("xOxO")
+        self.assertEqual(result, "Hugs and Kisses")
 
 
 if __name__ == "__main__":
